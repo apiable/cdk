91b2f684ad5b925d9b4d193add9c4126e5fe84c2
Alexander Schamne <alexander.schamne@gmail.com>
2024-04-17 10:52:02 +0100

    update readme$

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 1. Install CDK
 If it is the first time running CDK on AWS, then you need to install the CDK toolkit following the instructions here: [https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html)
 
-2. dd
+2. Export the paramaters
+```bash
+export AWS_ACCOUNT_ID=<your_account_id>
+export AWS_REGION=<your_region>
+export POOLNAME=<your_pool_name>
+export FROM_EMAIL=<your_from_email>
+export SES_VERIFIED_DOMAIN=<your_ses_verified_domain>
+```
+3. Deploy the stack
+```bash
+./deploy-cognito.sh
+```
--- deploy-cognito.sh
+++ deploy-cognito.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 checkMandatoryParameter () {
-  if [ -z "$1" ]
+  if [ -z "$1" ] && [ -z "$2" ]
     then
-      echo $2
+      echo $3
       echo "$EXAMPLE"
       exit 0
   fi
 }
 
 EXAMPLE="./deploy-cognito.sh 034444869755 eu-central-1 staging no-reply@apiable.io apiable.io"
-checkMandatoryParameter "$1" "aws-accountid must be set" && AWS_ACCOUNTID=$1
-checkMandatoryParameter "$2" "aws-region must be set" && AWS_REGION=$2
-checkMandatoryParameter "$3" "poolname must be set" && STACKNAME=$3
-checkMandatoryParameter "$4" "from email must be set" && FROM_EMAIL=$4
-checkMandatoryParameter "$5" "ses-verified-domain must be set" && SES_VERIFIED_DOMAIN=$5
+checkMandatoryParameter "$1" "$AWS_ACCOUNTID" "aws-accountid must be passed as a first parameter or exported through environment variable 'export AWS_ACCOUNTID=1234'" && AWS_ACCOUNTID=$1
+checkMandatoryParameter "$2" "$AWS_REGION" "aws-region must be passed as a second parameter or exported through environment variable 'export AWS_REGION=eu-central-1'" && AWS_REGION=$2
+checkMandatoryParameter "$3" "$POOLNAME" "poolname must be passed as a third parameter or exported through environment variable 'export POOLNAME=staging'" && POOLNAME=$3
+checkMandatoryParameter "$4" "$FROM_EMAIL" "from email must be passed as a fourth parameter or exported through environment variable 'export FROM_EMAIL=no-reply@apiable.io'" && FROM_EMAIL=$4
+checkMandatoryParameter "$5" "$SES_VERIFIED_DOMAIN" "ses-verified-domain must be passed as a fifth parameter or exported through environment variable 'export SES_VERIFIED_DOMAIN=apiable.io'" && SES_VERIFIED_DOMAIN=$5
 
 CDK_BIN_FILE="bin/apiable-cdk.ts"
 rm $CDK_BIN_FILE
@@ -35,6 +35,7 @@ new Cognito(app, "Cognito", {
 })
 EOT
 CONTEXT_OPTS="--context stackname=$STACKNAME --context from-email=$FROM_EMAIL --context ses-verified-domain=$SES_VERIFIED_DOMAIN --require-approval never --outputs-file ./cdk-outputs.json"
-cdk synth -q $CONTEXT_OPTS
-cdk deploy $CONTEXT_OPTS
+echo $CONTEXT_OPTS
+#cdk synth -q $CONTEXT_OPTS
+#cdk deploy $CONTEXT_OPTS
 
