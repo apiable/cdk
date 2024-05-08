3c473695a86f086d794f664c52b36499917a5ab9
Alexander Schamne <alexander.schamne@gmail.com>
2024-05-08 16:31:35 +0200

    eslint fix

--- bin/apiable-cdk.ts
+++ bin/apiable-cdk.ts
@@ -4,10 +4,10 @@ import { AuthZ } from '../lib/authz'
 const app = new cdk.App()
 // eslint-disable-next-line no-new
 new AuthZ(app, "AuthZ", {
-    stackName: "auth-portal-authz-dev",
-    description: "AuthZ Lambda for Apiable Gateway Authorization dev",
+    stackName: "auth-portal-authz-demo",
+    description: "AuthZ Lambda for Apiable Gateway Authorization demo",
     env: {
         account: "034444869755",
-        region: "eu-central-1"
+        region: "eu-north-1"
     }
 })
