873aabd2464c7f53e15c1255cd3c3be9e0a41ebd
Alexander Schamne <alexander.schamne@gmail.com>
2024-05-09 17:59:51 +0200

    add zip job

--- bin/apiable-cdk.ts
+++ bin/apiable-cdk.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib'
-import { AuthZ } from '../lib/authz'
+import { Cognito } from '../lib/cognito'
 
 const app = new cdk.App()
 // eslint-disable-next-line no-new
-new AuthZ(app, "AuthZ", {
-    stackName: "auth-portal-authz-demo",
-    description: "AuthZ Lambda for Apiable Gateway Authorization demo",
+new Cognito(app, "Cognito", {
+    stackName: "auth-portal-demo2",
+    description: "Cognito Pool for Apiable demo2 Portal",
     env: {
         account: "034444869755",
-        region: "eu-north-1"
+        region: "eu-central-1"
     }
 })
