095bae449a52d5f42ea1ab016e8306fcbd7c643b
Alexander Schamne <alexander.schamne@gmail.com>
2024-05-13 12:01:36 +0200

    add more pattern

--- bin/apiable-cdk.ts
+++ bin/apiable-cdk.ts
@@ -4,8 +4,8 @@ import { Cognito } from '../lib/cognito'
 const app = new cdk.App()
 // eslint-disable-next-line no-new
 new Cognito(app, "Cognito", {
-    stackName: "auth-portal-ixordocs",
-    description: "Cognito Pool for Apiable ixordocs Portal",
+    stackName: "auth-portal-ziffdigital",
+    description: "Cognito Pool for Apiable ziffdigital Portal",
     env: {
         account: "034444869755",
         region: "eu-central-1"
