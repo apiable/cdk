ffffb953abf25a942561653b8a47e935314e60dc
Alexander Schamne <alexander.schamne@gmail.com>
2024-06-28 12:12:46 +0200

    separate lambda with cognito

--- bin/apiable-cdk.ts
+++ bin/apiable-cdk.ts
@@ -4,10 +4,10 @@ import { AuthZ } from '../lib/authz'
 const app = new cdk.App()
 // eslint-disable-next-line no-new
 new AuthZ(app, "AuthZ", {
-    stackName: "auth-portal-authz-collectability",
-    description: "AuthZ Lambda for Apiable Gateway Authorization collectability",
+    stackName: "auth-portal-authz-dev",
+    description: "AuthZ Lambda for Apiable Gateway Authorization dev",
     env: {
         account: "034444869755",
-        region: "eu-west-1"
+        region: "eu-central-1"
     }
 })
