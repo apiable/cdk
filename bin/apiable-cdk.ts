6e126fea29cce5a735f9570013db15e2131aca58
Alexander Schamne <alexander.schamne@gmail.com>
2024-07-09 13:39:59 +0200

    add hybrid

--- bin/apiable-cdk.ts
+++ bin/apiable-cdk.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib'
-import { UsageLogs } from '../lib/usagelogs'
+import { AuthZ } from '../lib/authz'
 
 const app = new cdk.App()
 // eslint-disable-next-line no-new
-new UsageLogs(app, "UsageLogs", {
-    stackName: "usagelogs-apiable-dev",
-    description: "Usage Logs for Apiable Portal dev",
+new AuthZ(app, "AuthZ", {
+    stackName: "auth-portal-authz-developer",
+    description: "AuthZ Lambda for Apiable Gateway Authorization developer",
     env: {
         account: "034444869755",
         region: "eu-west-1"
